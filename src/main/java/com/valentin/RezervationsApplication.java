73fcd940a1f162e60955bbe9fa255363acff1fce
nedeloiu <valinedeloiu@gmail.com>
2018-11-04 00:45:07 +0200

    First commit

---
+++ src/main/java/com/valentin/RezervationsApplication.java
@@ -0,0 +1,12 @@
+package com.valentin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezervationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezervationsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/valentin/RezervationsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.valentin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RezervationsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
